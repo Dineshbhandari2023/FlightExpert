871b4be2eaf2090fe9aba71b66f4c18cc454bebe
Dinesh Bhandari <133768059+Dineshbhandari2023@users.noreply.github.com>
2024-07-18 17:52:11 +0000

    Reviewed

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,15 @@
-// import React from "react";
-// import FlightSearchForm from "./components/FlightSearchForm";
 
-// function App() {
-//   return (
-//     <div className="App">
-//       <FlightSearchForm />
-//     </div>
-//   );
-// }
-
-// export default App;
 import React from "react";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ModifySearch from "./components/ModifySearch";
-// import FlightSearchForm from "./components/FlightSearchForm";
+
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/search" element={<ModifySearch />} />
       </Routes>
     </Router>
